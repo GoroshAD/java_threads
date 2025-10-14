14587ed17caeb9db98873e43e6443b9b37d55e2e
Artiomka Goroschko <nikto8748@gmail.com>
2025-10-14 14:02:04 +0300

    add implemented

--- H5/First/PhoneBook/src/main/java/PhoneBook.java
+++ H5/First/PhoneBook/src/main/java/PhoneBook.java
@@ -1,5 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    static Map<String, String> phoneToNumber = new HashMap<>();
+    static Map<String, String> numberToPhone = new HashMap<>();
+
     public static int add(String name, String number) {
-        return 0;
+        phoneToNumber.put(name, number);
+        numberToPhone.put(number, name);
+        return phoneToNumber.size();
     }
 }
